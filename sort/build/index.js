0320499517244f81872e462f978f9499b1cace85
Glenn Pinto <gvpinto@gmail.com>
2024-02-14 22:41:31 -0500

    Implemented NumbersCollections finally

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,26 +1,8 @@
 "use strict";
-class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
-    sort() {
-        const { length } = this.collection;
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-                if (typeof this.collection === 'string') {
-                }
-            }
-        }
-    }
-}
-;
-const sorter = new Sorter([-1, 10, 3, 0, -5]);
+Object.defineProperty(exports, "__esModule", { value: true });
+const NumbersCollection_1 = require("./NumbersCollection");
+const Sorter_1 = require("./Sorter");
+const numbersCollection = new NumbersCollection_1.NumbersCollection([-1, 4, 10, 3, 0, -5]);
+const sorter = new Sorter_1.Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter);
+console.log(numbersCollection.data);
