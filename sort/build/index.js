974ea77903436d01202b3339d60072149da70bc4
Glenn Pinto <gvpinto@gmail.com>
2024-02-14 22:36:09 -0500

    Added NumbersCollections

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,6 +1,26 @@
 "use strict";
-console.log('123');
-const logSomething = () => {
-    console.log('logging');
-};
-logSomething();
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        const leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+                if (typeof this.collection === 'string') {
+                }
+            }
+        }
+    }
+}
+;
+const sorter = new Sorter([-1, 10, 3, 0, -5]);
+sorter.sort();
+console.log(sorter);
--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -1,5 +1,7 @@
+import { NumberCollection } from "./NumberCollections";
+
 export class Sorter {
-    constructor(public collection: number[] | string) { }
+    constructor(public collection: NumberCollection) { }
 
     sort() {
 
@@ -7,17 +9,8 @@ export class Sorter {
 
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection instanceof Array) {
-
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-
-                if (typeof this.collection === 'string') {
-
+                if (this.collection.compare(j, j + 1)) {
+                    this.collection.swap(i, j + 1);
                 }
             }
         }
