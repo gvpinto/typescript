964afbeae8e75c2b5070b68b14483fd7014c9b77
Glenn Pinto <gvpinto@gmail.com>
2024-02-14 22:48:40 -0500

    Completed CharactersCollection Sorting

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,9 +1,16 @@
 import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from "./Sorter";
+import CharactersCollection from "./charactersCollection";
 
-const numbersCollection = new NumbersCollection([-1, 4, 10, 3, 0, -5]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([-1, 4, 10, 3, 0, -5]);
+// const sorter = new Sorter(numbersCollection);
 
+// sorter.sort();
+
+// console.log(numbersCollection.data);
+
+
+const charactersCollection = new CharactersCollection('Xaaya');
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
